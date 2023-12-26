981293228086bb35f497bb2acdc2c11cdd9057b9
abdulhakim123 <75448160+abdulhakim123@users.noreply.github.com>
2023-12-26 10:47:26 +0300

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 15:46:54 2023
+
+@author: admin
+"""
+
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegressionCV
+iris=sns.load_dataset("iris")
+X=iris.values[:,:4]
+y=iris.values[:,4]
+train_X,test_X,train_y,test_y=train_test_split(X,y,train_size=0.5,test_size=0.5,random_state=0)
+lr=LogisticRegressionCV()
+lr.fit(train_X,train_y)
+print("Accuracy={:2f}".format(lr.score(test_X,test_y)))
